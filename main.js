358cbad90e69846ef6d8e373da67d76a71932c85
adnan80-hub <adnanzahm05@gmail.com>
2026-05-10 13:24:18 +0100

    room homepage master

---
+++ main.js
@@ -0,0 +1,40 @@
+let bar = document.querySelector(".bar");
+let closes = document.querySelector(".closes");
+let uls = document.querySelector("ul");
+
+
+
+
+
+bar.onclick = function () {
+    uls.style.cssText = "height: 75px;";
+}
+
+closes.onclick = function () {
+    uls.style.cssText = "height: 0;"
+}
+
+let arrowRight = document.querySelectorAll(".ang-right");
+let arrowLeft = document.querySelectorAll(".ang-left");
+
+arrowRight.forEach((ele) => {
+    ele.addEventListener("click", function () {
+        let head_page = document.querySelector(".parent-page .head-page.dis-flex");
+
+        if (head_page && head_page.nextElementSibling) {
+            head_page.classList.remove("dis-flex");
+            head_page.nextElementSibling.classList.add("dis-flex");
+        }
+    })
+})
+
+
+arrowLeft.forEach((ele) => {
+    ele.addEventListener("click", function () {
+        let head_page = document.querySelector(".parent-page .head-page.dis-flex");
+        if (head_page && head_page.previousElementSibling) {
+            head_page.classList.remove("dis-flex");
+            head_page.previousElementSibling.classList.add("dis-flex");
+        }
+    })
+})
