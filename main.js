50ddae52a54c067ef75ad954c9b30aa405167432
adnan80-hub <adnanzahm05@gmail.com>
2026-05-13 16:31:06 +0100

    another one

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const arrowLeft = document.querySelectorAll(".ang-left");
 
 arrowRight.forEach((ele) => {
     ele.addEventListener("click", function () {
-        let head_page = document.querySelector(".parent-page .head-page.dis-flex");
+        const head_page = document.querySelector(".parent-page .head-page.dis-flex");
 
         if (head_page && head_page.nextElementSibling) {
             head_page.classList.remove("dis-flex");
@@ -29,7 +29,7 @@ arrowRight.forEach((ele) => {
 
 arrowLeft.forEach((ele) => {
     ele.addEventListener("click", function () {
-        let head_page = document.querySelector(".parent-page .head-page.dis-flex");
+        const head_page = document.querySelector(".parent-page .head-page.dis-flex");
         if (head_page && head_page.previousElementSibling) {
             head_page.classList.remove("dis-flex");
             head_page.previousElementSibling.classList.add("dis-flex");
